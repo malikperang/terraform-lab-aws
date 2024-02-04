f4f13f2166b0f032fe898af86e17caff468dc765
justanothercloudguy <salam@farizizwan.com>
2024-02-04 19:36:35 +0800

    enab,e region

--- dev/providers.tf
+++ dev/providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-# provider "aws" {
-#   region = "ap-southeast-1"
-#   # shared_config_files = var.TFE_WORKSPACE_NAME != "" ? [] : ["~/.aws/config"]
-#   # profile = "farizizwan" #IAM Roles
-# }
+provider "aws" {
+  region = "ap-southeast-1"
+  # shared_config_files = var.TFE_WORKSPACE_NAME != "" ? [] : ["~/.aws/config"]
+  # profile = "farizizwan" #IAM Roles
+}
