5131d593dce3970ff09ee96c2b14e140e17f28d9
justanothercloudguy <salam@farizizwan.com>
2024-02-04 18:20:59 +0800

    test

--- dev/vars.tf
+++ dev/vars.tf
@@ -1,21 +1,11 @@
-# vars.tf
-
-variable "region" {
-  description = "AWS region where EC2 instance will be provisioned"
-  default     = "us-west-2"
+variable "tag_name" {
+    description = "Name tag for the resources"
+    type        = string
+    default     = "devops-showcase-jan2024"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
-}
-
-variable "ami" {
-  description = "ID of the Amazon Machine Image (AMI) to use for the EC2 instance"
-  default     = "ami-12345678" # Replace with your desired AMI ID
-}
-
-variable "subnet_id" {
-  description = "ID of the subnet where the EC2 instance will be deployed"
-  # Set a default or specify when calling Terraform
+variable "tag_project" {
+    description = "Project tag for the resources"
+    type        = string
+    default     = "devops-showcase-jan2024"
 }
